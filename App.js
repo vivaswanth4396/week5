2c06980b1535545fed9b101f5210cfe403448783
vardhan <puppalavivas@gmail.com>
2024-10-16 22:39:21 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const app=express();
+const port=3000;
+app.get("/",(req,res)=>{
+    res.send("<h1>hello world</h1>");
+});
+app.listen(port,()=>{
+    console.log(`app listening to the port ${port}`);
+});
